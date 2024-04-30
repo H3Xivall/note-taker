690b36170b538b64af0f592be6e8fd9a777b2d7d
Ryan Hanzel <100760416+H3Xivall@users.noreply.github.com>
2024-04-30 09:04:26 -0500

    coded the server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+// Import the express module for the server
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Sets up the express app and port the server will run on
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Routes
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/public/notes.html');
+});
+
+app.listen(PORT, () => {
+    console.log('App listening at http://localhost:' + PORT + '/');
+});
